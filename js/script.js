c87593eea6c9f9eca63cb6b90796d612d002d2f6
Irfanul Karim <karimirfanul10@gmail.com>
2020-07-24 07:26:29 +0600

    JS types part 1&2

--- js/script.js
+++ js/script.js
@@ -1,23 +1,42 @@
-// Defining Variable and function Part 1&2
+// // Defining Variable and function Part 1&2
 
-var x= "Hello";
-//   
-console.log(x);
+// var x= "Hello";
+// //   
+// console.log(x);
 
 
-var message = "global message";
-console.log("the value is "+message);
+// var message = "global message";
+// console.log("the value is "+message);
 
-var a = function(){
-    var message="irfanul";
-    console.log(message);
+// var a = function(){
+//     var message="irfanul";
+//     console.log(message);
 
-    var b=function(){
-        console.log(message);
-    }
-    b();
+//     var b=function(){
+//         console.log(message);
+//     }
+//     b();
+// }
+// a();
+
+// JS Types part 1&2
+
+var y;
+function a(){
+    console.log(y);
 }
 a();
 
+if(y==undefined){
+    console.log("y undefined");
+}
 
+y=5;
+if(y==undefined){
+    console.log("undefined");
+}
+else{
+    console.log("defined");
+}
 
+console.log(z);
