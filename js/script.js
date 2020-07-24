235e39eb5ee04f221196522bdd915f3556c2681e
Irfanul Karim <karimirfanul10@gmail.com>
2020-07-24 07:50:29 +0600

    Common language construct part 1,2,3

--- js/script.js
+++ js/script.js
@@ -21,22 +21,69 @@
 
 // JS Types part 1&2
 
-var y;
-function a(){
-    console.log(y);
-}
-a();
+// var y;
+// function a(){
+//     console.log(y);
+// }
+// a();
 
-if(y==undefined){
-    console.log("y undefined");
-}
+// if(y==undefined){
+//     console.log("y undefined");
+// }
 
-y=5;
-if(y==undefined){
-    console.log("undefined");
+// y=5;
+// if(y==undefined){
+//     console.log("undefined");
+// }
+// else{
+//     console.log("defined");
+// }
+
+// console.log(z);
+
+// Common language construct part 1,2,3
+
+var string = "hello";
+string+="world";
+console.log(string);
+
+console.log(4/2);
+console.log(undefined/0);
+
+function test (a){
+    console.log(a/4);
+}
+test();
+
+var a="2" ,b=2;
+if (a==b){
+    console.log("equal");
+}
+if(a===b){
+    console("equal");
 }
 else{
-    console.log("defined");
+    console.log("not equal");
 }
 
-console.log(z);
+if(false||undefined||null||0||""||NaN){
+    
+}
+else{
+    console.log("false");
+}
+
+if(true&&1&&-1&&"false"&&"hi"){
+    console.log("true");
+}
+
+// undefined 
+// {} shouldnot line break
+function q()
+{
+    return
+    {
+        console.log("hi")
+    }
+}
+console.log(q());
