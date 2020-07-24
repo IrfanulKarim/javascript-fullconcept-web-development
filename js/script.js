8c43303ab605c749d7fb25b8b048f7103119f2d5
Irfanul Karim <karimirfanul10@gmail.com>
2020-07-24 07:18:03 +0600

    Defining Variable and function Part 1&2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
+// Defining Variable and function Part 1&2
+
 var x= "Hello";
-function a(){
-    console.log(x);
+//   
+console.log(x);
+
+
+var message = "global message";
+console.log("the value is "+message);
+
+var a = function(){
+    var message="irfanul";
+    console.log(message);
+
+    var b=function(){
+        console.log(message);
+    }
+    b();
 }
+a();
+
+
+
