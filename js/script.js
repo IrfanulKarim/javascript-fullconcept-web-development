4a4807892abb717ab43eeab55a5b1925b8be1eb8
Irfanul Karim <karimirfanul10@gmail.com>
2020-07-24 12:24:21 +0600

    create object using object

--- js/script.js
+++ js/script.js
@@ -19,6 +19,15 @@
 // }
 // a();
 
+
+// 
+// 
+// 
+// End 
+// 
+// 
+//
+
 // JS Types part 1&2
 
 // var y;
@@ -41,57 +50,147 @@
 
 // console.log(z);
 
+
+
+
+// 
+// 
+// 
+// End 
+// 
+// 
+//
+
+
+
+
 // Common language construct part 1,2,3
 
-var string = "hello";
-string+="world";
-console.log(string);
+// var string = "hello";
+// string+="world";
+// console.log(string);
 
-console.log(4/2);
-console.log(undefined/0);
+// console.log(4/2);
+// console.log(undefined/0);
 
-function test (a){
-    console.log(a/4);
-}
-test();
+// function test (a){
+//     console.log(a/4);
+// }
+// test();
 
-var a="2" ,b=2;
-if (a==b){
-    console.log("equal");
-}
-if(a===b){
-    console("equal");
-}
-else{
-    console.log("not equal");
-}
+// var a="2" ,b=2;
+// if (a==b){
+//     console.log("equal");
+// }
+// if(a===b){
+//     console("equal");
+// }
+// else{
+//     console.log("not equal");
+// }
 
-if(false||undefined||null||0||""||NaN){
+// if(false||undefined||null||0||""||NaN){
     
-}
-else{
-    console.log("false");
-}
+// }
+// else{
+//     console.log("false");
+// }
 
-if(true&&1&&-1&&"false"&&"hi"){
-    console.log("true");
-}
+// if(true&&1&&-1&&"false"&&"hi"){
+//     console.log("true");
+// }
 
-// undefined 
-// {} shouldnot line break
-function q()
-{
-    return
-    {
-        console.log("hi")
-    }
-}
-console.log(q());
 
-// handling default values
-function order(dish){
-    dish= dish || "fuska";
-    console.log("food is "+dish);
-}
-order("sinagar");
-order();
+// 
+// 
+// 
+// End 
+// 
+// 
+//
+
+
+// // undefined 
+// // {} shouldnot line break
+// function q()
+// {
+//     return
+//     {
+//         console.log("hi")
+//     }
+// }
+// console.log(q());
+
+
+// 
+// 
+// 
+// End 
+// 
+// 
+//
+
+// // handling default values
+// function order(dish){
+//     dish= dish || "fuska";
+//     console.log("food is "+dish);
+// }
+// order("sinagar");
+// order();
+
+
+// 
+// 
+// 
+// End 
+// 
+// 
+//
+
+// // hello will print 2 times
+// // because console.log in if, will print hello in console but it will not return any value 
+// // thats why it will be undefineds.
+// var w=10;
+// if (null || console.log("hello") || w>5 ){
+//     console.log("hello")
+// }
+
+// 
+// 
+// 
+// End 
+// 
+// 
+// 
+
+// Creating Object using new object
+
+var company=new Object();
+company.name="karim";
+company.address="Chittagong";
+
+console.log(company);
+
+company.employee=new Object();
+company.employee.name="irfan";
+console.log("Employee name is "+company.employee.name);
+
+company.$employeesalary=11000;
+console.log("Salary is "+company.$employeesalary);
+
+company["employeage"]=30;
+console.log("Age is "+company["employeage"]);
+
+console.log("Company name is "+company["name"]);
+
+var number="employeNumber";
+company["number"] = 35;
+console.log("Employee Number is "+company["number"]);
+
+// 
+// 
+// 
+// End 
+// 
+// 
+// 
